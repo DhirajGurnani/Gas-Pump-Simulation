9d763c307bb38d9b2135445e574c39364690b526
Satyateja Pothuru <satyateja27@gmail.com>
2017-05-09 23:13:04 -0700

    Small Error Fix

--- greenfoot/fuelState.java
+++ greenfoot/fuelState.java
@@ -4,6 +4,7 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
+import greenfoot.*;
 public class FuelState implements State 
 {
     // instance variables - replace the example below with your own
